ee6e30d64ce148241b2978be20c4de55a8c9c204
Ally Parker <ally.parker@red-gate.com>
2020-12-31 17:15:51 +0000

    Clang format

--- app/src/display/widgets/wpm_status.c
+++ app/src/display/widgets/wpm_status.c
@@ -18,10 +18,8 @@ static lv_style_t label_style;
 
 static bool style_initialized = false;
 
-void wpm_status_init()
-{
-    if (style_initialized)
-    {
+void wpm_status_init() {
+    if (style_initialized) {
         return;
     }
 
@@ -33,8 +31,7 @@ void wpm_status_init()
     lv_style_set_text_line_space(&label_style, LV_STATE_DEFAULT, 1);
 }
 
-void set_wpm_symbol(lv_obj_t *label, int wpm)
-{
+void set_wpm_symbol(lv_obj_t *label, int wpm) {
     char text[4] = {};
 
     LOG_DBG("WPM changed to %i", wpm);
@@ -43,8 +40,7 @@ void set_wpm_symbol(lv_obj_t *label, int wpm)
     lv_label_set_text(label, text);
 }
 
-int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent)
-{
+int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent) {
     wpm_status_init();
     widget->obj = lv_label_create(parent, NULL);
     lv_obj_add_style(widget->obj, LV_LABEL_PART_MAIN, &label_style);
@@ -59,8 +55,7 @@ int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *p
 
 lv_obj_t *zmk_widget_wpm_status_obj(struct zmk_widget_wpm_status *widget) { return widget->obj; }
 
-int wpm_status_listener(const struct zmk_event_header *eh)
-{
+int wpm_status_listener(const struct zmk_event_header *eh) {
     struct wpm_state_changed *ev = cast_wpm_state_changed(eh);
     struct zmk_widget_wpm_status *widget;
     SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_wpm_symbol(widget->obj, ev->state); }
